5ec3d9c9b43eaaa981fd31b678c147b3d69ed7b9
josebastian1 <josefuentes123456789@gmail.com>
2024-03-16 01:05:05 -0300

    Juego funcional

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,45 @@
 let rondas = parseInt(prompt("Cuántas rondas quieres que tenga el juego?"));
-if (isNaN(rondas) || rondas <1){
+if (isNaN(rondas) || rondas < 1) {
     rondas = parseInt(prompt("Ingrese un número de rondas válido."));
 }
 
 let contadorUser = 0;
 let contadorCPU = 0;
 
-for (let i = 0; i < rondas; i++) {
-    let jugadas = ["piedra","papel","tijera"];
+function juego() {
+    for (let i = 0; i < rondas; i++) {
+        let jugadas = ["piedra", "papel", "tijera"];
 
-    let jugadaUser = prompt("Piedra, papel o tijera?").toLocaleLowerCase();
+        let jugadaUser = prompt("Piedra, papel o tijera?").toLocaleLowerCase();
 
-    let jugadaCPU = jugadas[Math.floor(Math.random()*3)];
+        let jugadaCPU = jugadas[Math.floor(Math.random() * 3)];
 
-    let resultadoRonda;
-    if ((jugadaUser === "piedra" && jugadaCPU === "tijera") || 
-        (jugadaUser === "papel" && jugadaCPU=== "piedra") ||
-        (jugadaUser === "tijera" && jugadaCPU=== "papel")){
+        let resultadoRonda;
+        if ((jugadaUser === "piedra" && jugadaCPU === "tijera") ||
+            (jugadaUser === "papel" && jugadaCPU === "piedra") ||
+            (jugadaUser === "tijera" && jugadaCPU === "papel")) {
             contadorUser++;
-            resultadoRonda = "Felicidades! Ganaste esta ronda!";
-        } else if (jugadaUser===jugadaCPU){
+            resultadoRonda = "Ganaste esta ronda!";
+        } else if (jugadaUser === jugadaCPU) {
             resultadoRonda = "Empate!";
         } else {
             contadorCPU++;
-            resultadoRonda = "Lastima! Perdiste esta ronda...";
+            resultadoRonda = "Pierdes esta ronda.";
         }
-        alert("La computadora elige: "+ jugadaCPU + resultadoRonda + `\n Marcador: \n Tú: `+ contadorUser + " - " + "Computadora: " + contadorCPU);
+        alert("La computadora elige " + jugadaCPU + "." + `\n` + resultadoRonda + `\nMarcador: \n Tú: ` + contadorUser + " - " + "Computadora: " + contadorCPU);
+    }
 }
+
+
+function final() {
+    let resultadoFinal = "El juego termina en empate.";
+    if (contadorUser > contadorCPU) {
+        resultadoFinal = "Felicidades, ganaste el juego!";
+    } else if (contadorUser < contadorCPU) {
+        resultadoFinal = "Gana la computadora! Suerte para la próxima..."
+    }
+    alert(resultadoFinal + "\nMarcador final: Tú " + contadorUser + " - " + contadorCPU + " Máquina")
+}
+
+juego();
+final();
